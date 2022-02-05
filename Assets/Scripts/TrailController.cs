177b28a052e4ac0274e45728c547c74d152b7259
MichaelSki82 <romantik1001@yandex.ru>
2022-02-05 16:55:26 +0300

    HW1
    Трейл Рендерер и добавление управления

---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,22 @@
+﻿using JoostenProductions;
+using UnityEngine;
+using System;
+
+public class TrailController:BaseController
+{
+    private TrailRendererView _trailView;
+
+    public TrailController(TrailRendererView trailView)
+    {
+        _trailView = trailView;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        position = new Vector3(position.x, position.y, 0);
+        _trailView.SetPosition(position);
+    }
+
+   
+
+}
